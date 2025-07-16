22eaa8d72cd1fa4727b22e0c294eee6a7c213839
Jai <anandjai440@gmail.com>
2025-07-16 18:44:47 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -24,12 +24,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
 connectToMongoDB().then(() => {
 	server.listen(PORT, () => {
 		console.log(`Server Running on port ${PORT}`);
