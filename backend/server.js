64a2d9d30ea42adee70c5da5f69c509701e7c85d
Jai <anandjai440@gmail.com>
2025-07-16 19:23:01 +0530

    Server.js updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,10 +14,13 @@ import { app, server } from "./socket/socket.js";
 dotenv.config({ path: "./backend/.env" });
 console.log("Loaded MONGO_DB_URI:", process.env.MONGO_DB_URI);
 
-
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+    origin: ["https://chatter-hjceh5giy-jai-anand-jas-projects.vercel.app"], 
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -24,8 +28,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
+
 connectToMongoDB().then(() => {
-	server.listen(PORT, () => {
-		console.log(`Server Running on port ${PORT}`);
-	});
+    server.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
