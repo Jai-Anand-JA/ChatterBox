7ce01bc00c44759862788543173fd506356a75e1
Jai <anandjai440@gmail.com>
2025-07-16 19:54:44 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,20 @@
-import path from "path";
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config({ path: "./backend/.env" });
 console.log("Loaded MONGO_DB_URI:", process.env.MONGO_DB_URI);
 
-const PORT = process.env.PORT || 5000;
-
-const allowedOrigins = [
-    "https://chatter-hjceh5giy-jai-anand-jas-projects.vercel.app",
-    "https://chatter-odrjm4jg9-jai-anand-jas-projects.vercel.app"
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
+    origin: "*"
 }));
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -40,6 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+const PORT = process.env.PORT || 5000;
 connectToMongoDB().then(() => {
     server.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
