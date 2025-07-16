78577f17f21e55c6a7d17b44787e1c6a480c3eed
Jai <anandjai440@gmail.com>
2025-07-16 19:34:50 +0530

    hooks updated

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -3,28 +3,28 @@ import { useAuthContext } from "../context/AuthContext";
 import toast from "react-hot-toast";
 
 const useLogout = () => {
-	const [loading, setLoading] = useState(false);
-	const { setAuthUser } = useAuthContext();
+    const [loading, setLoading] = useState(false);
+    const { setAuthUser } = useAuthContext();
 
-	const logout = async () => {
-		setLoading(true);
-		try {
-			console.log("API URL:", import.meta.env.VITE_API_URL);
-			const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/logout`, {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-			});
-			const data = await res.json();
-			if (data.error) throw new Error(data.error);
-			localStorage.removeItem("chat-user");
-			setAuthUser(null);
-		} catch (error) {
-			toast.error(error.message);
-		} finally {
-			setLoading(false);
-		}
-	};
+    const logout = async () => {
+        setLoading(true);
+        try {
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/logout`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+            });
+            const data = await res.json();
+            if (data.error) throw new Error(data.error);
 
-	return { loading, logout };
+            localStorage.removeItem("chat-user");
+            setAuthUser(null);
+        } catch (error) {
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { loading, logout };
 };
 export default useLogout;
