16d3a03c416fca4020aae6f304c1239e6e91a257
Jai <anandjai440@gmail.com>
2025-07-16 19:28:12 +0530

    Server.js updated

--- backend/server.js
+++ backend/server.js
@@ -14,25 +14,19 @@ import { app, server } from "./socket/socket.js";
 dotenv.config({ path: "./backend/.env" });
 console.log("Loaded MONGO_DB_URI:", process.env.MONGO_DB_URI);
 
-const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: ["https://chatter-hjceh5giy-jai-anand-jas-projects.vercel.app"], 
     credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
-});
 
 connectToMongoDB().then(() => {
     server.listen(PORT, () => {
